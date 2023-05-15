2a613cd5390d0f794e2e8efde7247643e223352b
HimanshiKhandelwal14 <133160944+HimanshiKhandelwal14@users.noreply.github.com>
2023-05-16 00:18:41 +0530

    Update mvn_file.java

--- mvn_file.java
+++ mvn_file.java
@@ -1,5 +1,16 @@
 package com.project;
 public class App
 {
+  public int userlogin(String in_user , String in_pwd)
+  {
+    resourceBundle rb=ResourceBundle.getBundle("config");
+    String userName=rb.getString("username");
+    String password=rb.getString("password");
+    
+    if(in_user.equals(username) && in_pwd.equals(password))
+      return1;
+    else
+      return0;
+  }
   
 }
